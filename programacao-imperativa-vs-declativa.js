9eca8bdf9bb5247a3787888d28f669a5529fb13c
mairess <trabalho.maires@gmail.com>
2023-04-18 08:37:40 -0300

    update programcao-imperativa-vs-declarativa.js

--- programacao-imperativa-vs-declativa.js
+++ programacao-imperativa-vs-declativa.js
@@ -1,9 +1,14 @@
 // Função que ela o número ao quadrado
 
-
-
 // Imperativo: Faça da seguinto forma
+let number = 2
 
+function square(){
+    return number * number
+}
+
+number = square()
 
 
 // Delcarativa: O que fazer e não como fazer
+const square = n => n * n
